a642169edd06c7e390b89151415a90620d7a0fb0
andrewosh <andrewosh@gmail.com>
2015-07-01 00:42:02 -0400

    Updated tests to use axes instead of split

--- test/spark/test_spark.py
+++ test/spark/test_spark.py
@@ -200,7 +200,7 @@ def test_getitem_slice_ragged(sc):
 
     x = arange(10*10*3).reshape((10, 10, 3))
 
-    b = array(x, sc, split=2)
+    b = array(x, sc, axes=(0,1))
     assert allclose(b[0:5:2, 0:2].toarray(), x[0:5:2, 0:2])
     assert allclose(b[0:5:3, 0:2].toarray(), x[0:5:3, 0:2])
     assert allclose(b[0:9:3, 0:2].toarray(), x[0:9:3, 0:2])
@@ -227,12 +227,12 @@ def test_getitem_list(sc):
 
     x = arange(3*3*4).reshape((3, 3, 4))
 
-    b = array(x, sc, split=1)
+    b = array(x, sc, axes=(0,))
     assert allclose(b[[0, 1], [0, 1], [0, 2]].toarray(), x[[0, 1], [0, 1], [0, 2]])
     assert allclose(b[[0, 1], [0, 2], [0, 3]].toarray(), x[[0, 1], [0, 2], [0, 3]])
     assert allclose(b[[0, 1, 2], [0, 2, 1], [0, 3, 1]].toarray(), x[[0, 1, 2], [0, 2, 1], [0, 3, 1]])
 
-    b = array(x, sc, split=2)
+    b = array(x, sc, axes=(0,1))
     assert allclose(b[[0, 1], [0, 1], [0, 2]].toarray(), x[[0, 1], [0, 1], [0, 2]])
     assert allclose(b[[0, 1], [0, 2], [0, 3]].toarray(), x[[0, 1], [0, 2], [0, 3]])
     assert allclose(b[[0, 1, 2], [0, 2, 1], [0, 3, 1]].toarray(), x[[0, 1, 2], [0, 2, 1], [0, 3, 1]])
@@ -245,8 +245,8 @@ def test_getitem_list_array(sc):
     cols = [[0, 2], [0, 2]]
     dept = [[0, 3], [0, 3]]
 
-    b = array(x, sc, split=1)
+    b = array(x, sc, axes=(0,))
     assert allclose(b[rows, cols, dept].toarray(), x[rows, cols, dept])
 
-    b = array(x, sc, split=2)
+    b = array(x, sc, axes=(0,1))
     assert allclose(b[rows, cols, dept].toarray(), x[rows, cols, dept])
