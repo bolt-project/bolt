f5f0a8f9e5742cde5bbf2c0f540dbff074f9e838
freeman <the.freeman.lab@gmail.com>
2015-07-05 11:28:54 -0400

    Change logging

--- test/conftest.py
+++ test/conftest.py
@@ -11,5 +11,8 @@ sys.path[:0] = [spark_python, py4j]
 @pytest.fixture(scope="session")
 def sc():
     from pyspark import SparkContext
-    return SparkContext(appName="bolt-tests")
+    sc = SparkContext(appName="bolt-tests")
+    log4j = sc._jvm.org.apache.log4j
+    log4j.LogManager.getRootLogger().setLevel(log4j.Level.ERROR)
+    return sc
 
