6db22ddb337d8dbd3f8336a06ece0d888293017e
freeman <the.freeman.lab@gmail.com>
2015-07-05 11:13:39 -0400

    Specify paths manually

--- test/conftest.py
+++ test/conftest.py
@@ -1,7 +1,12 @@
+import os
+import sys
+import glob
 import pytest
-import findspark
 
-findspark.init()
+spark_home = os.environ['SPARK_HOME']
+spark_python = os.path.join(spark_home, 'python')
+py4j = glob.glob(os.path.join(spark_python, 'lib', 'py4j-*.zip'))[0]
+sys.path[:0] = [spark_python, py4j]
 
 @pytest.fixture(scope="session")
 def sc():
