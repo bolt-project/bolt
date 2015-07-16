fa9f868b011a42527f264a9956b9c54b4d0073fe
jwittenbach <jason.wittenbach@gmail.com>
2015-07-15 23:44:24 -0400

    added simple unit tests

---
+++ test/spark/test_spark_chunking.py
@@ -0,0 +1,29 @@
+import pytest
+from numpy import arange, split
+from bolt import array
+from bolt.utils import allclose
+
+def test_chunk(sc):
+    
+    x = arange(4*6).reshape(1, 4, 6)
+    b = array(x, sc)
+    
+    k1, v1 = zip(*b.chunk((2,3))._rdd.sortByKey().collect())
+    k2 = tuple(zip(((0,), (0,), (0,), (0,)), ((0, 0), (0, 1), (1, 0), (1, 1))))
+    v2 = [s for m in split(x[0], (2,), axis=0) for s in split(m, (3,), axis=1)]
+    assert k1 == k2
+    assert all([allclose(m1, m2) for (m1, m2) in zip(v1, v2)])
+
+    k1, v1 = zip(*b.chunk((3,4))._rdd.sortByKey().collect())
+    k2 = tuple(zip(((0,), (0,), (0,), (0,)), ((0, 0), (0, 1), (1, 0), (1, 1))))
+    v2 = [s for m in split(x[0], (3,), axis=0) for s in split(m, (4,), axis=1)]
+    assert k1 == k2
+    assert all([allclose(m1, m2) for (m1, m2) in zip(v1, v2)])
+
+def test_unchunk(sc):
+
+    x = arange(4*6).reshape(1, 4, 6)
+    b = array(x, sc)
+
+    allclose(b.chunk((2,3)).unchunk().toarray(), b.toarray())
+    allclose(b.chunk((3,4)).unchunk().toarray(), b.toarray())
